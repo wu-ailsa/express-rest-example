793cc9265d2ac16c1795dd66568944a0c0bff2a1
wu-ailsa <ailsawunyc@gmail.com>
2023-11-09 11:35:42 -0500

    crud fix

--- index.js
+++ index.js
@@ -23,27 +23,65 @@ app.get("/api/users", (req, res) => {
 })
 
 //CREATE - POST - create a user
-app.post('/api/users', (req,res)=>{
-    if(req.body.name && req.body.username && req.body.email) {
-        if(user.find((u)=> u.username == req.body.username))
-    }
-})
+app.post("/api/users", (req, res) => {
+    if (req.body.name && req.body.username && req.body.email) {
+      if (users.find((u) => u.username == req.body.username)) {
+        res.json({ error: "Username Already Taken" });
+        return;
+      }
+  
+      const user = {
+        id: users[users.length - 1].id + 1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+  
+      users.push(user);
+      res.json(users[users.length - 1]);
+    } else res.json({ error: "Insufficient Data" });
+  });
+  
 
 //SHOW - GET - get one user
-app.get("/api/users/:id", (req, res,next) => {
+app.get("/api/users/:id", (req, res, next) => {
     //find the user id
-    const user = users.find((u)=> u.id == req.params.id);
-
+    const user = users.find((u) => u.id == req.params.id);
+  
     console.log(user);
-    //display the user in json
-    if(user)res.json(user);
-    else next()
-})
+    //if the user exists display the json data
+    if (user) res.json(user);
+    else next();
+  });
+  
 
-//UPDATE - PUT/PATCH - update a user
+//UPDATE - PUT/PATCH - update a user\
+app.patch("/api/users", (req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        for (const key in req.body) {
+          users[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+  
+    if (user) res.json(user);
+    else next();
+  });
 
 //DELETE - DELETE - delete a user from database
-
+app.delete("/api/users/:id", (req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        users.splice(i, 1);
+        return true;
+      }
+    });
+  
+    if (user) res.json(user);
+    else next();
+  });
 
 //-------------------------   POST ROUTES  ---------------------------//
 
@@ -52,7 +90,22 @@ app.get('/api/posts',(req, res) => {
     res.json(posts);
 })
 
-//CREATE - POST - add a new post to database 
+//CREATE - POST - add a new post to the database
+
+app.post("/api/posts", (req, res) => {
+    if (req.body.userId && req.body.title && req.body.content) {
+      const post = {
+        id: posts[posts.length - 1].id + 1,
+        userId: req.body.userId,
+        title: req.body.title,
+        content: req.body.content,
+      };
+  
+      posts.push(post);
+      res.json(posts[posts.length - 1]);
+    } else res.json({ error: "Insufficient Data" });
+  });
+
 //SHOW - GET - shows information of one post
 app.get('/api/posts/:id',(req,res,next)=>{
     const post = posts.find((p)=> p.id == req.params.id)
@@ -61,9 +114,34 @@ app.get('/api/posts/:id',(req,res,next)=>{
     else next()
 })
 
-//UPDATE - PUT/PATCH - update a particular post 
 
-//DELETE - DELETE - delete a particular post from database
+//UPDATE - PUT/PATCH - update a particular post
+app.patch("/api/posts/:id", (req, res, next) => {
+  const post = posts.find((p, i) => {
+    if (p.id == req.params.id) {
+      for (const key in req.body) {
+        posts[i][key] = req.body[key];
+      }
+      return true;
+    }
+  });
+
+  if (post) res.json(post);
+  else next();
+});
+
+//DELETE - DELETE - delete a particular post
+app.delete("/api/posts/:id", (req, res, next) => {
+    const post = posts.find((p, i) => {
+      if (p.id == req.params.id) {
+        posts.splice(i, 1);
+        return true;
+      }
+    });
+  
+    if (post) res.json(post);
+    else next();
+  });
 
 //custom middleware - 404 not found
 app.use((req,res)=>{
---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const users = require('../data/users')
+
+
+
+
+
+module.exports = router;
